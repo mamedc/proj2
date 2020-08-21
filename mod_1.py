b064abcd92fb31be251a6f4d047ca42772cc1cab
mamed <mamedcaki@gmail.com>
2020-08-21 16:52:07 -0300

    commit do proj 2

---
+++ mod_1.py
@@ -0,0 +1,17 @@
+# Created on 2020-08-13  12:31 from C:\Users\Mamed\Projects\proj1\mod_1.ipynb
+
+import numpy as np
+
+def even_odd_printer(x):
+    '''
+    Testa se é par ou ímpar.
+    x, int
+    '''
+    assert isinstance(x, int), 'Argument must be a integer!'
+    if x % 2 == 0:
+        return 'even'
+    return 'odd'
+	
+# alterado
+
+# alterado feature-1
